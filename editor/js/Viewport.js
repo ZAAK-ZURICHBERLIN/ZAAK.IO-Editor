470f4ec8fb908eb5c0dba51b0578ddde6c97043c
elephantatwork <design@elephantatwork.ch>
2015-10-08 08:06:28 +0200

    Reimplemented online dummy

--- editor/js/Viewport.js
+++ editor/js/Viewport.js
@@ -29,8 +29,8 @@ var Viewport = function ( editor ) {
 	// load a resource
 	loader.load(
 		// resource URL
-		// DUMMY,
-		'3D/dummy.json',
+		DUMMY,
+		// '3D/dummy.json',
 		// Function when resource is loaded
 		function ( geometry, materials ) {
 			vrHuman = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial( ) );
