a4bb57fb4da34f2dbed22b5f8de4a948764628a0
elephantatwork <design@elephantatwork.ch>
2015-10-14 15:11:07 +0200

    Revert "Merge branch 'master' into zaak_dev"
    This reverts commit 0ec1d67ab06a846a212c369392ab4d40eaa7e3f3, reversing
    changes made to be1a6698601c53557681ed8c9131407e0bc65591.

--- build/three.js
+++ build/three.js
@@ -12844,17 +12844,7 @@ THREE.Loader.prototype = {
 
 			}
 
-<<<<<<< HEAD
 			var material = new THREE[ mtype ]( mpars );
-=======
-		//@elephantatZAAK
-		//for PhongMaterial with map
-		if( json.map !== undefined) material.map = json.map;
-
-		// for PointCloudMaterial
-		if ( json.size !== undefined ) material.size = json.size;
-		if ( json.sizeAttenuation !== undefined ) material.sizeAttenuation = json.sizeAttenuation;
->>>>>>> master
 
 			if ( m.DbgName !== undefined ) material.name = m.DbgName;
 
@@ -13054,14 +13044,7 @@ THREE.ImageLoader.prototype = {
 
 				onProgress( event );
 
-<<<<<<< HEAD
 			}, false );
-=======
-				
-
-
-			};
->>>>>>> master
 
 		}
 
@@ -13075,17 +13058,9 @@ THREE.ImageLoader.prototype = {
 
 		if ( this.crossOrigin !== undefined ) image.crossOrigin = this.crossOrigin;
 
-<<<<<<< HEAD
 		scope.manager.itemStart( url );
 
 		image.src = url;
-=======
-					material.map = getTexture( data.map );
-					
-					//Load Texture from Main Folder (( Pretty UGLY ))
-					material.map = load(data.map);
-				}
->>>>>>> master
 
 		return image;
 
@@ -13814,26 +13789,11 @@ THREE.MaterialLoader = function ( manager ) {
 
 };
 
-<<<<<<< HEAD
 THREE.MaterialLoader.prototype = {
-=======
-
-		if ( this.name !== "" ) output.name = this.name;
->>>>>>> master
 
 	constructor: THREE.MaterialLoader,
 
-<<<<<<< HEAD
 	load: function ( url, onLoad, onProgress, onError ) {
-=======
-			output.color = this.color.getHex();
-			if ( this.vertexColors !== THREE.NoColors ) output.vertexColors = this.vertexColors;
-			if ( this.blending !== THREE.NormalBlending ) output.blending = this.blending;
-			if ( this.side !== THREE.FrontSide ) output.side = this.side;
-			
-
-			
->>>>>>> master
 
 		var scope = this;
 
@@ -13845,18 +13805,7 @@ THREE.MaterialLoader.prototype = {
 
 		}, onProgress, onError );
 
-<<<<<<< HEAD
 	},
-=======
-			//@elephantatZAAK
-			//Add diffuseMap to json Export
-			if(this.map != null) {
-				//console.log(this.map.sourceFile);
-				output.map = this.map.sourceFile;
-			}
-
-		} else if ( this instanceof THREE.MeshNormalMaterial ) {
->>>>>>> master
 
 	setCrossOrigin: function ( value ) {
 
